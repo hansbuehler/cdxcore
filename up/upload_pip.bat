0168c0aa3f812782ebad09c0662b76b2e29e227f
Hans Buehler <github@hans.buehler.london>
2025-09-11 12:11:44 +0100

    Code base 0.1.14

--- up/upload_pip.bat
+++ up/upload_pip.bat
@@ -1,52 +1,48 @@
+@echon off
+echo ** PIP: to avoid having to provide a PyPI API key, modify $HOME/.pypirc **
+REM ** PIP follows https://packaging.python.org/tutorials/packaging-projects/ **
 
-echo =====================================================================================
-echo PIP build
-echo To avoid having to provide a PyPI API key, modify $HOME/.pypirc
-echo =====================================================================================
-
-REM https://packaging.python.org/tutorials/packaging-projects/
 cd C:\Users\hans\OneDrive\Python3\packages\cdxcore
-
 CALL .\up\test.bat
 
-REM: always exists because of 'test' 
-REM: call python -m venv .vcdxcore
+@echo off
 if not exist .vcdxcore echo "*** ERROR no ENVIRONMENT ***"
 if not exist .vcdxcore exit 2 
 
 if exist dist rmdir /Q /S dist
 mkdir dist
 call .vcdxcore\Scripts\activate
-python -m -y pip install -U pip twine build
 
-REM ** create distribution **
-pip install -q -U twine build
+@echo on
+python -m pip install -qq -U pip --no-input
+pip install -qq -U twine build --no-input
 python up\pip_modify_setup.py 
 python -m build
 python -m twine upload dist\*
+
+@echo off
 rmdir /Q /S dist
+REM ** Install from pypi **
 
-REM test pip install
-pip --no-input uninstall -q cdxcore
-pip -y install --upgrade cdxcore
+@echo on
+pip uninstall -qq cdxcore --no-input
+pip install --upgrade cdxcore --no-input
+@echo off
 
-echo =====================================================================================
-echo GIT upload
-echo Uses cdxcore version to set git message
-echo =====================================================================================
+echo ** GIT upload **
 
-echo GIT upload
 python up\git_message.py >.tmp.txt
 set /p MESSAGE=< .tmp.txt
 del /q .tmp.txt
-REM echo Python test showed %MESSAGE%
+REM ==> echo Python test showed %MESSAGE%
+@echo on
+
 git commit -a -m "%MESSAGE%"
 git push
 
+@echo off
 call deactivate
 
-echo =====================================================================================
-echo cdxcore pip, git done
-echo =====================================================================================
+echo ** cdxcore testing, pip, git done **
 
 
